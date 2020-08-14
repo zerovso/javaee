d282fe68e6ff859f5b0e49d165097ec6b858f4cc
zeromo <zerovso@126.com>
2020-08-14 12:32:20 +0800

    # WARNING: head commit changed in the meantime
    初始化项目文件

---
+++ src/main/java/com/zerovso/javaee/App.java
@@ -0,0 +1,13 @@
+package com.zerovso.javaee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
